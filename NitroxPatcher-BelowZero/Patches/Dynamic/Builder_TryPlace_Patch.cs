ffed2fe75657703af9588e040212f08f72be2585
Onni Kukkonen <onni.kukkonen77@gmail.com>
2024-04-01 00:26:58 +0300

    building: works sound whitelist: works

--- NitroxPatcher-BelowZero/Patches/Dynamic/Builder_TryPlace_Patch.cs
+++ NitroxPatcher-BelowZero/Patches/Dynamic/Builder_TryPlace_Patch.cs
@@ -15,85 +15,85 @@ using static System.Reflection.Emit.OpCodes;
 
 namespace NitroxPatcher_BelowZero.Patches.Dynamic;
 
-//TODO: patch this in
-// public sealed partial class Builder_TryPlace_Patch : NitroxPatch, IDynamicPatch
-// {
-//     public static readonly MethodInfo TARGET_METHOD = Reflect.Method(() => Builder.TryPlace());
+public sealed partial class Builder_TryPlace_Patch : NitroxPatch, IDynamicPatch
+{
+    public static readonly MethodInfo TARGET_METHOD = Reflect.Method(() => Builder.TryPlace());
 
-//     public static readonly InstructionsPattern AddInstructionPattern1 = new()
-//     {
-//         Ldloc_0,
-//         Ldc_I4_0,
-//         Ldc_I4_1,
-//         new() { OpCode = Callvirt, Operand = new(nameof(Constructable), nameof(Constructable.SetState)) },
-//         { Pop, "Insert1" }
-//     };
+    public static readonly InstructionsPattern AddInstructionPattern1 = new()
+    {
+        Ldloc_0,
+        Ldc_I4_0,
+        Ldc_I4_1,
+        new() { OpCode = Callvirt, Operand = new(nameof(Constructable), nameof(Constructable.SetState)) },
+        { Pop, "Insert1" }
+    };
 
-//     public static readonly List<CodeInstruction> InstructionsToAdd1 = new()
-//     {
-//         new(Ldloc_0),
-//         new(Call, Reflect.Method(() => GhostCreated(default)))
-//     };
+    public static readonly List<CodeInstruction> InstructionsToAdd1 = new()
+    {
+        new(Ldloc_0),
+        new(Call, Reflect.Method(() => GhostCreated(default)))
+    };
 
-//     public static readonly InstructionsPattern AddInstructionPattern2 = new()
-//     {
-//         Ldloc_S,
-//         Ldloc_3,
-//         Ldloc_S,
-//         Or,
-//         { new() { OpCode = Callvirt, Operand = new(nameof(Constructable), nameof(Constructable.SetIsInside)) }, "Insert2" }
-//     };
+    public static readonly InstructionsPattern AddInstructionPattern2 = new()
+    {
+        Ldloc_S,
+        Ldloc_S,
+        Or,
+        { new() { OpCode = Callvirt, Operand = new(nameof(Constructable), nameof(Constructable.SetIsInside)) }, "Insert2" }
+    };
 
-//     public static readonly List<CodeInstruction> InstructionsToAdd2 = new()
-//     {
-//         TARGET_METHOD.Ldloc<Constructable>(),
-//         new(Call, Reflect.Method(() => GhostCreated(default)))
-//     };
+    public static readonly List<CodeInstruction> InstructionsToAdd2 = new()
+    {
+        TARGET_METHOD.Ldloc<GameObject>(),
+        new(Callvirt, Reflect.Method((UnityEngine.GameObject g) => g.GetComponentInParent<Constructable>())),
+        new(Call, Reflect.Method(() => GhostCreated(default)))
+    };
 
-//     public static IEnumerable<CodeInstruction> Transpiler(MethodBase original, IEnumerable<CodeInstruction> instructions) =>
-//         instructions.Transform(AddInstructionPattern1, (label, instruction) =>
-//         {
-//             if (label.Equals("Insert1"))
-//             {
-//                 return InstructionsToAdd1;
-//             }
-//             return null;
-//         }).Transform(AddInstructionPattern2, (label, instruction) =>
-//         {
-//             if (label.Equals("Insert2"))
-//             {
-//                 return InstructionsToAdd2;
-//             }
-//             return null;
-//         });
+    public static IEnumerable<CodeInstruction> Transpiler(MethodBase original, IEnumerable<CodeInstruction> instructions) =>
+        instructions.Transform(AddInstructionPattern1, (label, instruction) =>
+        {
+            if (label.Equals("Insert1"))
+            {
+                return InstructionsToAdd1;
+            }
 
-//     public static void GhostCreated(Constructable constructable)
-//     {
-//         GameObject ghostObject = constructable.gameObject;
+            return null;
+        }).Transform(AddInstructionPattern2, (label, instruction) =>
+        {            
+            if (label.Equals("Insert2"))
+            {
+                return InstructionsToAdd2;
+            }
+            return null;
+        });
 
-//         NitroxId parentId = null;
-//         if (ghostObject.TryGetComponentInParent(out SubRoot subRoot, true) && (subRoot.isBase || subRoot.isCyclops) &&
-//             subRoot.TryGetNitroxId(out NitroxId entityId))
-//         {
-//             parentId = entityId;
-//         }
+    public static void GhostCreated(Constructable constructable)
+    {
+        GameObject ghostObject = constructable.gameObject;
 
-//         // Assign a NitroxId to the ghost now
-//         NitroxId ghostId = new();
-//         NitroxEntity.SetNewId(ghostObject, ghostId);
-//         if (constructable is ConstructableBase constructableBase)
-//         {
-//             GhostEntity ghost = GhostEntitySpawner.From(constructableBase);
-//             ghost.Id = ghostId;
-//             ghost.ParentId = parentId;
-//             Resolve<IPacketSender>().Send(new PlaceGhost(ghost));
-//         }
-//         else
-//         {
-//             ModuleEntity module = ModuleEntitySpawner.From(constructable);
-//             module.Id = ghostId;
-//             module.ParentId = parentId;
-//             Resolve<IPacketSender>().Send(new PlaceModule(module));
-//         }
-//     }
-// }
+        NitroxId parentId = null;
+        if (ghostObject.TryGetComponentInParent(out SubRoot subRoot, true) && (subRoot.isBase || subRoot.isCyclops) &&
+            subRoot.TryGetNitroxId(out NitroxId entityId))
+        {
+            parentId = entityId;
+        }
+
+        // Assign a NitroxId to the ghost now
+        NitroxId ghostId = new();
+        NitroxEntity.SetNewId(ghostObject, ghostId);
+        if (constructable is ConstructableBase constructableBase)
+        {
+            GhostEntity ghost = GhostEntitySpawner.From(constructableBase);
+            ghost.Id = ghostId;
+            ghost.ParentId = parentId;
+            Resolve<IPacketSender>().Send(new PlaceGhost(ghost));
+        }
+        else
+        {
+            ModuleEntity module = ModuleEntitySpawner.From(constructable);
+            module.Id = ghostId;
+            module.ParentId = parentId;
+            Resolve<IPacketSender>().Send(new PlaceModule(module));
+        }
+    }
+}
--- NitroxPatcher-BelowZero/Patches/Dynamic/SubNameInput_OnNameChange_Patch.cs
+++ NitroxPatcher-BelowZero/Patches/Dynamic/SubNameInput_OnNameChange_Patch.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Reflection;
 using NitroxClient_BelowZero.GameLogic;
 using NitroxClient_BelowZero.Unity.Helper;
@@ -20,28 +21,41 @@ public sealed partial class SubNameInput_OnNameChange_Patch : NitroxPatch, IDyna
 
     public static bool TryGetTargetId(SubNameInput subNameInput, out object target, out NitroxId targetId)
     {
-        SubName subName = (SubName)subNameInput.target;
-        if (!subName)
+        ICustomizeable subName = subNameInput.target;
+        Log.Info($"TryGetTargetId for type {subNameInput.target.GetType()}");
+        
+        if (subName == null)
         {
             target = null;
             targetId = null;
             return false;
         }
-        if (subName.TryGetComponent(out Vehicle vehicle))
-        {
-            target = vehicle;
-            return vehicle.TryGetNitroxId(out targetId);
-        }
-        else if (subName.TryGetComponentInParent(out Rocket rocket, true))
-        {
-            // For some reason only the rocket has a full functioning ghost with a different NitroxId when spawning/constructing, so we are ignoring it.
-            if (rocket.TryGetComponentInChildren(out VFXConstructing constructing, true) && !constructing.isDone)
+
+        if (subName is ColorNameControl colorNameControl) {
+            if (colorNameControl.gameObject.TryGetComponent(out Vehicle vehicle))
             {
-                target = null;
-                targetId = null;
-                return false;
+                target = vehicle;
+                return vehicle.TryGetNitroxId(out targetId);
+            }
+            else if (colorNameControl.gameObject.TryGetComponent(out Rocket rocket))
+            {
+                // For some reason only the rocket has a full functioning ghost with a different NitroxId when spawning/constructing, so we are ignoring it.
+                if (rocket.TryGetComponentInChildren(out VFXConstructing constructing, true) && !constructing.isDone)
+                {
+                    target = null;
+                    targetId = null;
+                    return false;
+                }
+            } else if (colorNameControl.gameObject.TryGetComponent(out SeaTruckSegment seaTruckSegment)) {
+                target = seaTruckSegment;
+                return seaTruckSegment.TryGetNitroxId(out targetId);
+            } else if (colorNameControl.gameObject.TryGetComponent(out Hoverbike hoverbike)) {
+                target = hoverbike;
+                return hoverbike.TryGetNitroxId(out targetId);
             }
         }
+
+       
         // Cyclops and Rocket has their SubNameInput and SubName in the same GameObject, marked with a NitroxEntity
         target = subNameInput;
         return subNameInput.TryGetNitroxId(out targetId);
